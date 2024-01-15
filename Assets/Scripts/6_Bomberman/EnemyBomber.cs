22ea86f8fcb4365f6a0310edff0adb7956486ab5
El_Espectro <jaespinosag@esimurcia.com>
2024-01-15 19:21:50 +0100

    [Giro] Programando la IA de giro.

--- Assets/Scripts/6_Bomberman/EnemyBomber.cs
+++ Assets/Scripts/6_Bomberman/EnemyBomber.cs
@@ -17,6 +17,10 @@ public class EnemyBomber : MonoBehaviour {
     [field: SerializeField] private float sideOffset { get; set; } = 1f;
     private float boundsSize { get; set; }
     //private float sideSphereRadius { get; set; }
+    [field: SerializeField, Range(0, 100)] private int rightChance { get; set; } = 33;
+    [field: SerializeField, Range(0, 100)] private int leftChance { get; set; } = 33;
+    [field: SerializeField] private float turnCooldown { get; set; } = 0.5f;
+    private bool canTurn { get; set; } = true;
 	
     void Start() {
         boundsSize = render.bounds.extents.x;
@@ -39,19 +43,75 @@ public class EnemyBomber : MonoBehaviour {
     }
 
     void DetectSides() {
+        if (!canTurn)
+            return;
+
         bool _right = Physics.CheckSphere(transform.position + transform.right * sideOffset, boundsSize, obstacleLayer);
         bool _left = Physics.CheckSphere(transform.position - transform.right * sideOffset, boundsSize, obstacleLayer);
 
-        if (_right) {
-            transform.eulerAngles -= new Vector3(0, 90, 0); // Rotate to the left
-        } else if (_left) {
+        int _random = Random.Range(1, 101);
+
+        if (!_right && !_left) {
+            canTurn = false;
+            if (_random <= rightChance) {
+                transform.eulerAngles += new Vector3(0, 90, 0); // Rotate to the right
+            } else if (_random <= rightChance + leftChance) {
+                transform.eulerAngles -= new Vector3(0, 90, 0); // Rotate to the left
+            }
+            StartCoroutine(TurnCooldownCo());
+        }
+        else if (!_right) {
+            TurnRightSimple(_random);
+            //TurnRightProfesor();
+        } else if (!_left) {
+            TurnLeftSimple(_random);
+            //TurnLeftProfesor();
+        }
+    }
+
+    void TurnRightProfesor() {
+        canTurn = false;
+        int _random = Random.Range(1, 101);
+        if (_random <= rightChance) {
             transform.eulerAngles += new Vector3(0, 90, 0); // Rotate to the right
         }
+        StartCoroutine(TurnCooldownCo());
+    }
+
+    void TurnLeftProfesor() {
+        canTurn = false;
+        int _random = Random.Range(1, 101);
+        if (_random <= leftChance) {
+            transform.eulerAngles -= new Vector3(0, 90, 0); // Rotate to the left
+        }
+        StartCoroutine(TurnCooldownCo());
+    }
+
+    void TurnRightSimple(int _random) {
+        canTurn = false;
+        if (_random <= rightChance) {
+            transform.eulerAngles += new Vector3(0, 90, 0); // Rotate to the right
+        }
+        StartCoroutine(TurnCooldownCo());
+    }
+
+    void TurnLeftSimple(int _random) {
+        canTurn = false;
+        if (_random <= rightChance + leftChance) {
+            transform.eulerAngles -= new Vector3(0, 90, 0); // Rotate to the left
+        }
+        StartCoroutine(TurnCooldownCo());
+    }
+
+    IEnumerator TurnCooldownCo() {
+        yield return new WaitForSeconds(turnCooldown);
+        canTurn = true;
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position, transform.forward * frontRayLength);
         Gizmos.DrawWireSphere(transform.position + transform.right * sideOffset, boundsSize);
+        Gizmos.DrawWireSphere(transform.position - transform.right * sideOffset, boundsSize);
     }
 }
